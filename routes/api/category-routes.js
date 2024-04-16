43856c5682878ac060f66305fb690937f58ea6a0
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-04-16 12:11:51 -0500

    created route to get all categoires and products. route to find a category by id and its products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,24 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // Find all categories and include associated Products
+  Category.findAll({ include: Product })
+    .then(categories => res.json(categories))
+    .catch(error => res.status(500).json(error));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, { include: Product })
+  .then(category => {
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.json(category);
+  })
+  .catch(error => res.status(500).json(error));
 });
 
 router.post('/', (req, res) => {
