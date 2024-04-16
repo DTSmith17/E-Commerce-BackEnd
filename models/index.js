d224b3ba90a161d6e71a154b7f28322d67ceb840
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-04-16 15:05:11 -0500

    added associations for tables

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category)
 
 // Categories have many Products
-
+Category.hasMany(Products)
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {
+  through: 'ProductTag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Products.belongsToMany(Products, {
+  through: 'productsTag'
+})
 
 module.exports = {
   Product,
